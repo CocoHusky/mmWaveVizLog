ad7816326caddd3b381c0e29c6f18edf9ab7bd1b
Alex Burton <82059352+CocoHusky@users.noreply.github.com>
2026-06-24 23:23:54 -0500

    Add Arduino private config example

---
+++ arduino/MR60BHA2_Sensor_VisLog/vislog_private_config.example.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: MIT */
+
+#ifndef VISLOG_PRIVATE_CONFIG_EXAMPLE_H
+#define VISLOG_PRIVATE_CONFIG_EXAMPLE_H
+
+/*
+ * Copy this file to vislog_private_config.h for local builds.
+ * vislog_private_config.h is ignored by Git so private lab or device
+ * credentials do not get committed to the public repository.
+ */
+#define VISLOG_WIFI_AP_PASSWORD "change-this-wifi-password"
+#define VISLOG_OTA_PASSWORD "change-this-ota-password"
+
+#endif
