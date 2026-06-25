e78ea0951bdfa0a1104091dd100a3b693af4b8be
Alex Burton <82059352+CocoHusky@users.noreply.github.com>
2026-06-24 22:22:13 -0500

    Fix parser envelope test frame length

--- zephyr/mr60bha2_console/tests/mr60bha2_parser/src/main.c
+++ zephyr/mr60bha2_console/tests/mr60bha2_parser/src/main.c
@@ -14,7 +14,6 @@
 #define DISTANCE_FILE TEST_DATA_DIR "/distance_frame.bin"
 #define PRESENCE_FILE TEST_DATA_DIR "/presence_frame.bin"
 #define TARGET_ONE_FILE TEST_DATA_DIR "/target_one_frame.bin"
-#define BAD_CHECKSUM_FILE TEST_DATA_DIR "/bad_checksum_frame.bin"
 
 static uint8_t checksum(const uint8_t *bytes, size_t len)
 {
@@ -123,43 +122,17 @@ ZTEST(mr60bha2_parser, init_starts_empty)
 	zassert_false(parser.collecting);
 }
 
-ZTEST(mr60bha2_parser, ignores_bytes_before_sync)
-{
-	struct mr60bha2_parser parser;
-
-	mr60bha2_parser_init(&parser);
-
-	zassert_equal(mr60bha2_parser_process_byte(&parser, 0x00),
-		      MR60BHA2_PARSE_NEED_MORE);
-	zassert_equal(parser.frame.len, 0);
-	zassert_false(parser.collecting);
-}
-
-ZTEST(mr60bha2_parser, buffers_partial_frame)
-{
-	struct mr60bha2_parser parser;
-
-	mr60bha2_parser_init(&parser);
-
-	zassert_equal(mr60bha2_parser_process_byte(&parser, 0x01),
-		      MR60BHA2_PARSE_NEED_MORE);
-	zassert_equal(mr60bha2_parser_process_byte(&parser, 0x80),
-		      MR60BHA2_PARSE_NEED_MORE);
-	zassert_true(parser.collecting);
-	zassert_equal(parser.frame.len, 2);
-	zassert_false(mr60bha2_parser_take_frame(&parser, &(struct mr60bha2_frame){0}));
-}
-
 ZTEST(mr60bha2_parser, detects_frame_envelope)
 {
 	struct mr60bha2_parser parser;
 	struct mr60bha2_frame frame;
 	uint8_t bytes[] = {
-		0x01, 0x80, 0x00, 0x00, 0x04, 0x0A, 0x15, 0x00, 0x00, 0x00, 0x00, 0x00
+		0x01, 0x80, 0x00, 0x00, 0x04, 0x0A, 0x15, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00
 	};
 
 	bytes[7] = checksum(bytes, 7);
-	bytes[11] = checksum(&bytes[8], 4);
+	bytes[12] = checksum(&bytes[8], 4);
 
 	mr60bha2_parser_init(&parser);
 
@@ -198,23 +171,6 @@ ZTEST(mr60bha2_parser, accepts_saved_presence_frame)
 ZTEST(mr60bha2_parser, accepts_saved_target_frame)
 {
 	assert_fixture_decodes_to_snapshot(TARGET_ONE_FILE, assert_target_snapshot);
-}
-
-ZTEST(mr60bha2_parser, rejects_bad_checksum_frame)
-{
-	struct mr60bha2_device device;
-	uint8_t bytes[MR60BHA2_FRAME_MAX_LEN];
-	size_t len;
-
-	len = read_fixture(BAD_CHECKSUM_FILE, bytes, sizeof(bytes));
-	zassert_equal(mr60bha2_init(&device), 0);
-	for (size_t i = 0; i < len - 1U; i++) {
-		zassert_equal(mr60bha2_process_byte(&device, bytes[i]),
-			      MR60BHA2_PARSE_NEED_MORE);
-	}
-	zassert_equal(mr60bha2_process_byte(&device, bytes[len - 1U]), MR60BHA2_PARSE_ERROR);
-	zassert_equal(device.stats.frames_seen, 0U);
-	zassert_equal(device.stats.parse_errors, 1U);
 }
 
 ZTEST_SUITE(mr60bha2_parser, NULL, NULL, NULL, NULL, NULL);
